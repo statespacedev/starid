0e4a9931e8c131d1fba36f7dfc65b5ea62eef15c
Noah Smith <smith@vesseltracker.com>
2018-08-17 12:08:02 +0200

    sync

--- starid/__main__.py
+++ starid/__main__.py
@@ -3,11 +3,12 @@ sys.path.append('starid')
 
 def main():
     parser = argparse.ArgumentParser('starid')
-    parser.add_argument('-t', help='shows a test star image', action='store_true')
+    parser.add_argument('-t', help='test the project', action='store_true')
     args = parser.parse_args()
+
     if args.t:
-        import test
-        test.test()
+        import util
+        util.image()
 
 if __name__ == "__main__":
     main()
--- starid/util.py
+++ starid/util.py
@@ -9,15 +9,12 @@ def image_info_to_image(image_info):
         image[int(image_info[rowndx,0]), int(image_info[rowndx,1])] = 1.0
     return image
 
-def test():
+def image():
     ls.load_sky()
     image_info = ls.image_info(3)
     image = image_info_to_image(image_info)
     plt.matshow(-1 * image, cmap='Greys', interpolation='nearest')
     plt.show()
 
-def main():
-    pass
-
 if __name__ == "__main__":
-    main()
+    pass
