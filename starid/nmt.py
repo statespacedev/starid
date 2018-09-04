f68b4c5c2d0550979ce89210af8113dda5961a79
Noah Smith <noahhsmith@gmail.com>
2018-09-05 00:03:08 +0200

    sync

--- starid/nmt.py
+++ starid/nmt.py
@@ -116,13 +116,14 @@ def train_minimalist():
     global_step = 0
     while global_step < hparams.num_train_steps:
         try:
-            (_, step_loss, step_predict_count, step_summary, global_step, step_word_count, batch_size) = loaded_train_model.train(train_sess)
+            #(_, step_loss, step_predict_count, step_summary, global_step, step_word_count, batch_size) = loaded_train_model.train(train_sess)
+            output = loaded_train_model.train(train_sess)
             global_step += 1
         except tf.errors.OutOfRangeError:
             utils.print_out('# epoch completed, step %d' % global_step)
             train_sess.run(train_model.iterator.initializer, feed_dict={train_model.skip_count_placeholder: 0})
             continue
-    summary_writer.add_summary(step_summary, global_step)
+    #summary_writer.add_summary(step_summary, global_step)
     loaded_train_model.saver.save(train_sess, os.path.join(hparams.out_dir, 'checkpoint.ckpt'), global_step=global_step)
     summary_writer.close()
 
