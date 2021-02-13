0a9890512ca1f3c42d83ea83adaeabffa0173fa7
Noah Smith <smith@vesseltracker.com>
2021-02-13 22:21:29 +0100

    sync

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+import os, sys
+sys.path.insert(0, os.path.abspath('../starid/demo.py'))
+project = 'Starid'
+copyright = '2021, Noah Smith'
+author = 'Noah Smith'
+# release = '1.3.22'
+# extensions = ['sphinx_rtd_theme']
+templates_path = ['_templates']
+exclude_patterns = []
+# html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
