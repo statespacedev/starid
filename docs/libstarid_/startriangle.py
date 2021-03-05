d161a90e043e9ccde80a0045aa8ff228238b2dfb
Noah Smith <smith@vesseltracker.com>
2021-03-05 07:09:38 +0100

    sync

--- docs/libstarid_/startriangle.py
+++ docs/libstarid_/startriangle.py
@@ -1,5 +1,7 @@
 class StartriangleNOMAD:
-   '''NOMAD triangle.'''
+   '''NOMAD triangle. focus is on the basestar and baseside - nomad is about a chain of basesides, each increasing the constraints on the preceding basestars. closing the loop around a triangle for each baseside speeds up that process.'''
+   def isrecognized(self):
+      '''true if basestars and basesides have been constrained to the point where only one possible basestar remains.'''
 class StartriangleSETTLER:
    '''SETTLER triangle. acts as the triangles abca and abda within the star triangle identifier inner loops. their are three triangle sides - representing three star pairs, each with an angular separation. each side is acted by a star triangle side object.'''
    def close_loops_abda(self, triangles):
