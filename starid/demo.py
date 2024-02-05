16972b7c8e25bff6a9a6aadbcb07f72f4e42806d
Noah Smith <nsmith>
2024-02-05 09:47:38 +0100

    bring include code inside project

--- starid/demo.py
+++ starid/demo.py
@@ -5,20 +5,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import pprint
+from starid.util import get_project_root
 
-# sys.path.append('/tmp/cmake-build-debug/libstarid')
-sys.path.append('./cmake-build-debug/libstarid')
-
-
-# # -DPYBIND11_FINDPYTHON=ON  # initial cmake options on macbook
-# # -DPYBIND11_PYTHON_VERSION=3.10  # final cmake options on macbook
+dirext = get_project_root() + '/cmake-build-debug/libstarid'
+print(dirext)
+sys.path.append(dirext)
+os.add_dll_directory(dirext)
 
 # noinspection PyUnresolvedReferences
 class Demo:
     """handle calls to libstarid.cpp."""
 
     def __init__(self):
-        self.dirdata = './tmp/data/'
+        self.dirdata = get_project_root() + '/tmp'
         self.filecat = 'cat'
         self.filesky = 'sky'
         self.filestarpairs = 'starpairs'
--- starid/tests/test_demo.py
+++
@@ -1,6 +0,0 @@
-from starid.demo import Demo
-
-
-def test_data():
-    demo = Demo()
-    assert demo.check_data()
