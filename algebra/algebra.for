e0a3a57c0c011074644223f4f71294284915133a
Noah Smith <noah@statespace.dev>
2025-03-22 08:43:35 +0100

    troubleshoot

--- algebra/algebra.for
+++ algebra/algebra.for
@@ -7,10 +7,11 @@ c this matches with feynman's table 22-3.
       y = .00225
       x = sqrt(1. - y**2)
       do 20 i=1,11
-        write (nout, 10), x, y
+        write (nout, 10) x, y
         x2 = x**2 - y**2
         y2 = 2. * x * y
         x = x2
         y = y2
 20    continue
+      stop
       end
