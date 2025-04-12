8db914191ac3f3ef0d507b3826718ff181a35e59
Noah Smith <noah@statespace.dev>
2025-04-12 05:50:16 +0200

    trek folder

--- dec10/tape-decwar.sh
+++ dec10/tape-decwar.sh
@@ -3,17 +3,17 @@
 rm -rf ./decwar-files
 mkdir ./decwar-files
 
-cp ~/decwar/utexas/*.* ./decwar-files
-cp ~/decwar/utexas/include/*.* ./decwar-files
-cp ~/decwar/compuserve/*.* ./decwar-files
+cp ../trek/utexas/*.* ./decwar-files
+cp ../trek/utexas/include/*.* ./decwar-files
+cp ../trek/compuserve/*.* ./decwar-files
 
-cp ~/decwar/utexas/hlp/DECWAR.HLP ./decwar-files
-cp ~/decwar/utexas/hlp/DECWAR.NWS ./decwar-files
+cp ../trek/utexas/hlp/DECWAR.HLP ./decwar-files
+cp ../trek/utexas/hlp/DECWAR.NWS ./decwar-files
 
-cp ~/decwar/scripts/COM1.CMD ./decwar-files
-cp ~/decwar/scripts/COM2.CMD ./decwar-files
-cp ~/decwar/scripts/CAN1.CMD ./decwar-files
+cp ../trek/scripts/COM1.CMD ./decwar-files
+cp ../trek/scripts/COM2.CMD ./decwar-files
+cp ../trek/scripts/CAN1.CMD ./decwar-files
 
-./back10 -cf ./decwar.tap ./decwar-files/*
-./back10 -lf ./decwar.tap
+./back10 -cf ./tapes/decwar.tap ./decwar-files/*
+./back10 -lf ./tapes/decwar.tap
 
