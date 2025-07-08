0aeb6ab77334693944354a51dde153c50a587710
Noah Smith <noah@statespace.dev>
2025-07-09 01:04:07 +0200

    test.sh

--- dec10/robots/test.sh
+++ dec10/robots/test.sh
@@ -1,5 +1,8 @@
 # use tail -f to watch one of the log files
-
+rm log*
+./msc/create-tape-utexas23-reconstruction
+./dec10blinken simh/boot-from-disk.ini > log0 &
+sleep 20
 python -u robots/run nomad > log1 &
 sleep 10
 python -u robots/run robot2 > log2 &
