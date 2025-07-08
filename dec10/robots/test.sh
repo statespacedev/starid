9811f3756c805260887a95245a5eec49fc15b56b
Noah Smith <noah@statespace.dev>
2025-07-09 01:20:17 +0200

    test.sh

--- dec10/robots/test.sh
+++ dec10/robots/test.sh
@@ -5,14 +5,14 @@ rm log*
 ./dec10blinken simh/boot-from-disk.ini > log0 &
 sleep 30
 python -u robots/run nomad > log1 &
-sleep 10
+sleep 20
 python -u robots/run robot2 > log2 &
-sleep 10
+sleep 20
 python -u robots/run robot3 > log3 &
-sleep 10
+sleep 20
 python -u robots/run robot4 > log4 &
-sleep 10
+sleep 20
 python -u robots/run robot5 > log5 &
-sleep 10
+sleep 20
 python -u robots/run robot6 > log6 &
 
